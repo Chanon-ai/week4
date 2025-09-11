ab06824b90d0a42f0ffc0cc4d9475d9ed1d8fa23
Chanon <6631501029@lamduan.mfu.ac.th>
2025-09-11 16:51:41 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/week5/design2.dart';
+import 'package:flutter_application_1/week5/random.dart';
 
 void main() {
-  runApp(MaterialApp(home: Design2(), debugShowCheckedModeBanner: false,));
+  runApp(MaterialApp(home: RandomDemo(), debugShowCheckedModeBanner: false,));
 }
