1275f8efc695791790f0b131f4c2c7cce0f57449
Chanon <6631501029@lamduan.mfu.ac.th>
2025-09-18 15:06:38 +0700

    img

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/week6/week6.dart';
+import 'package:flutter_application_1/week6/week6.3.dart';
 
 
 void main() {
-  runApp(MaterialApp(home: ButtonDemo(), debugShowCheckedModeBanner: false,));
+  runApp(MaterialApp(home: WelcomeScreen(), debugShowCheckedModeBanner: false,));
 }
