3999051410f3d4ff54d5334606ef3bf6d035a6a9
Chanon <6631501029@lamduan.mfu.ac.th>
2025-09-24 20:41:45 +0700

    Ex6

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/week6/Ass1.dart';
-import 'package:flutter_application_1/week6/Ass2.dart';
-import 'package:flutter_application_1/week6/Ex1.dart';
-import 'package:flutter_application_1/week6/Ex2.dart';
-import 'package:flutter_application_1/week6/Ex3.dart';
-import 'package:flutter_application_1/week6/Ex4.dart';
-import 'package:flutter_application_1/week6/Ex5.dart';
-import 'package:flutter_application_1/week6/week6.3.dart';
-import 'package:flutter_application_1/week6/week6.4.dart';
+import 'package:flutter_application_1/week6/Ex6.dart';
 
 void main() {
-  runApp(MaterialApp(home: TestInput(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: TestApp2(), debugShowCheckedModeBanner: false));
 }
---
+++ lib/week6/Ex6.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestApp2 extends StatefulWidget {
+  const TestApp2({super.key});
+
+  @override
+  State<TestApp2> createState() => _TestApp2State();
+}
+
+class _TestApp2State extends State<TestApp2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
